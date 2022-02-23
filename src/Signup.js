7601e52d7898d0a68af32a2705eaf55e00b9f3b1
Julian <jufiejuf@gmail.com>
2022-02-23 11:53:11 +0800

    routing next

--- src/Signup.js
+++ src/Signup.js
@@ -1,17 +1,37 @@
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from './contexts/AuthContext'
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const {signup} = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        if (passwordRef.current.value !== passwordConfirmRef.current.value){
+            return setError('Incorrect Password')
+        }
+
+        try{
+            setError("")
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Failed to create account')
+        }
+    }
 
     return (
     <>
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-3">Sign Up </h2>
-                <Form>
+                {error && <Alert variant = "danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit} >
                     <Form.Group id = "email">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required />
@@ -24,7 +44,7 @@ export default function Signup() {
                         <Form.Label>Password Confirmation</Form.Label>
                         <Form.Control type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
-                    <Button className="w-100 text-center mt-3" type="submit">
+                    <Button disabled={loading} className="w-100 text-center mt-3" type="submit">
                         Sign Up
                     </Button>
                 </Form>
