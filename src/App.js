81f4f0fa8eb5b820826f125cc1756d8fa49952e6
Raco <racopostmeridian@gmail.com>
2022-02-25 15:48:56 +0800

    Button adjustments
    + Create curriculum button now uses an image.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import Modal from './modal';
 import './modal.css';
+import createButton from './button_create.png';
 
 function App() {
   const [show, setShow] = useState(false)
   return (
-    <div className="App">
-      <button onClick ={() => setShow(true)}> Create a Curriculum </button>
+    <div className="App"
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+  }}>
+        <img src={createButton}
+        alt = "Create my Curriculum"
+        onClick ={() => setShow(true)}/> 
       <Modal title = "Create a New Curriculum" onClose = {() => setShow(false)} show = {show}>
         <p> Please add form content here tysm :3 </p>  
       </Modal>
