66c936d9f26562cc785e78b38e26e21cae5b8f9c
VDNaraval <vdnaraval@gmail.com>
2022-03-28 09:46:13 +0800

    added edit course

---
+++ src/components/EditCourseModal.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from 'react';
+import BaseDropdown from './BaseDropdown';
+import BaseForm from './BaseForm';
+import BaseInput from './BaseInput';
+import BaseModal from './BaseModal';
+
+const EditCourseModal = ({ show, setShowEditCourseModal, editCourse, courseTitle, courseCode, courseUnits, courseGrade }) => {
+	const codeInputRef = useRef();
+	const titleInputRef = useRef();
+	const unitsInputRef = useRef();
+	const gradeInputRef = useRef();
+
+	const clearInputs = () => {
+		codeInputRef.current.value = courseCode;
+		titleInputRef.current.value = courseTitle;
+		unitsInputRef.current.value = courseUnits;
+		gradeInputRef.current.value = courseGrade;
+	};
+
+	const submitCourse = (e) => {
+		e.preventDefault();
+        editCourse(
+            titleInputRef.current.value, 
+            codeInputRef.current.value, 
+            unitsInputRef.current.value, 
+            gradeInputRef.current.value
+        );
+		closeModal();
+	};
+
+	const closeModal = (e) => {
+		clearInputs();
+		setShowEditCourseModal(false);
+	};
+
+    useEffect(() => {
+		if (codeInputRef) codeInputRef.current.value = courseCode;
+		if (titleInputRef) titleInputRef.current.value = courseTitle;
+		if (unitsInputRef) unitsInputRef.current.value = courseUnits;
+		if (gradeInputRef) gradeInputRef.current.value = courseGrade;
+	}, [courseCode, courseTitle, courseUnits, courseGrade, show]);
+
+	return (
+		<BaseForm onSubmit={submitCourse}>
+			<BaseModal
+				show={show}
+				title={'Course Creator'}
+				hasCancelButton
+				hasActionButton
+				actionIsSubmit
+				onClose={closeModal}>
+				<BaseInput label='Title' required ref={titleInputRef} />
+				<BaseInput label='Code' required ref={codeInputRef} />
+				<BaseInput label='Units' type='number' required ref={unitsInputRef} />
+				<BaseDropdown
+					label='Course Grade'
+					options={[
+						'1.00',
+						'1.25',
+						'1.50',
+						'1.75',
+						'2.00',
+						'2.25',
+						'2.50',
+						'2.75',
+						'3.00',
+						'4.00',
+						'5.00',
+						'INC',
+						'DRP',
+					]}
+					ref={gradeInputRef}
+				/>
+			</BaseModal>
+		</BaseForm>
+	);
+};
+
+export default EditCourseModal;
