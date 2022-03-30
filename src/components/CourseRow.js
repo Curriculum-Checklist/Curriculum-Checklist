29574d85ccc9407c177172c41468d15a879ded87
Julian <jufiejuf@gmail.com>
2022-03-30 22:11:59 +0800

    status button clickable

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -6,6 +6,16 @@ const themeColors = ['red', 'yellow', 'green', 'blue', 'orange'];
 const CourseRow = ({ course, index }) => {
 	const color = themeColors[index % themeColors.length];
 
+function changeStatus() {
+	if (course.status === "Not Taken") {
+		course.status = "Taking";
+	} else if (course.status === "Taking"){
+		course.status = "Taken";
+	} else if (course.status === "Taken"){
+		course.status = "Not Taken";
+	}
+}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.leftOuter}>
@@ -15,9 +25,9 @@ const CourseRow = ({ course, index }) => {
 					<p className={styles.title}>{course.title}</p>
 				</div>
 			</div>
-			<div className={styles.middle}>
+			<button type="button" onClick={changeStatus} className={styles.middle}>
 				<p className={styles.status}>{course.status}</p>
-			</div>
+			</button>
 			<div className={styles.right}>
 				<p className={styles.units}>{course.units}</p>
 			</div>
