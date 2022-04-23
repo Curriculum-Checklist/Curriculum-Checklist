0ef95f0de2be6c96cb3d5817589eb22ae1579486
VDNaraval <vdnaraval@gmail.com>
2022-04-23 11:56:49 +0800

    added gwa

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,6 +16,7 @@ import { useFirestore } from '../contexts/FirestoreContext';
 import EditCurriculumInfoModal from '../components/EditCurriculumInfoModal';
 import EditCourseModal from '../components/EditCourseModal';
 import EditSemInfoModal from '../components/EditSemInfoModal';
+import { PieChart } from 'react-minimal-pie-chart';
 
 const editingCurriculum = new Curriculum();
 
@@ -101,7 +102,91 @@ export default function Dashboard() {
 	//TODO - use dropdown to notify user on failed logout
 	if (!curriculum) {
 		return <div className={styles.container}>{!curriculum && <GetCurriculumBanner />}</div>;
+	};
+	
+	// For GWA Computation
+	var GWA = 0;
+	var GradePerUnit = 0;
+	var TotalUnits = 0;
+	var g100 = 0;
+	var g125 = 0;
+	var g150 = 0;
+	var g175 = 0;
+	var g200 = 0;
+	var g225 = 0;
+	var g250 = 0;
+	var g275 = 0;
+	var g300 = 0;
+	var g400 = 0;
+	var g500 = 0;
+
+	const computeGWA = () => {
+		sems.map((sem) => (
+			sem.courses.map((course) => (
+				ifValidGrade(course)
+			))
+		))
+		GWA = GradePerUnit / TotalUnits;
 	}
+
+	const ifValidGrade = (course) => {
+		if (['1.00', '1.25', '1.50', '1.75', '2.00', '2.25', '2.50', '2.75', '3.00', '4.00', '5.00'].includes(course.grade) &&
+			course.requiredGrade === true) {
+			GradePerUnit = GradePerUnit + (parseFloat(course.grade) * parseFloat(course.units));
+			TotalUnits = TotalUnits + parseFloat(course.units);
+
+			if (course.grade === '1.00') {
+				g100 = g100 + 1;
+			}
+
+			if (course.grade === '1.25') {
+				g125 = g125 + 1;
+			}
+			
+			if (course.grade === '1.50') {
+				g150 = g150 + 1;
+			}
+			
+			if (course.grade === '1.75') {
+				g175 = g175 + 1;
+			}
+			
+			if (course.grade === '2.00') {
+				g200 = g200 + 1;
+			}
+			
+			if (course.grade === '2.25') {
+				g225 = g225 + 1;
+			}
+			
+			if (course.grade === '2.50') {
+				g250 = g250 + 1;
+			}
+			
+			if (course.grade === '2.75') {
+				g275 = g275 + 1;
+			}
+			
+			if (course.grade === '3.00') {
+				g300 = g300 + 1;
+			}
+			
+			if (course.grade === '4.00') {
+				g400 = g400 + 1;
+			}
+			
+			if (course.grade === '5.00') {
+				g500 = g500 + 1;
+			}
+			
+		}
+	}
+
+	const hasGrade = (dataEntry) => {
+		if (dataEntry.value > 0) {
+			return (dataEntry.title)
+		}
+	} 
 
 	return (
 		<DashboardContext.Provider
@@ -159,7 +244,40 @@ export default function Dashboard() {
 						</div>
 					)}
 				</div>
-
+				
+				<div className={styles.viewGWACard}>
+					<div>
+					<p className={styles.gwaTitle}> GWA </p>
+					{computeGWA()}
+					<p className={styles.gwaValue}> {Math.round(GWA * 100) / 100} </p>
+					</div>
+					<div className={styles.gwaChart}>
+						<PieChart
+							data={[
+								{ title: '1.00', value: g100, color: 'red'},
+								{ title: '1.25', value: g125, color: 'blue'},
+								{ title: '1.50', value: g150, color: 'green'},
+								{ title: '1.75', value: g175, color: 'orange'},
+								{ title: '2.00', value: g200, color: 'yellow'},
+								{ title: '2.25', value: g225, color: 'purple'},
+								{ title: '2.50', value: g250, color: 'pink'},
+								{ title: '2.75', value: g275, color: 'brown'},
+								{ title: '3.00', value: g300, color: 'white'},
+								{ title: '4.00', value: g400, color: 'gray'},
+								{ title: '5.00', value: g500, color: 'black'},
+							]}
+							lineWidth={15}
+							label={({ dataEntry }) => hasGrade(dataEntry)}
+							labelStyle={() => ({
+								fill: 'white',
+								fontSize: '10px',
+								fontFamily: 'sans-serif',
+							})}
+							radius={25}
+							labelPosition={120}
+						/>
+					</div>
+				</div>
 				<EditCurriculumInfoModal onSave={updateCurriculumInfo} />
 				<EditSemInfoModal onSave={updateSemInfo} />
 				<EditCourseModal onSave={updateCourse} />
